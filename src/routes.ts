b60030372f7cd1fd7fd03528dd9179f845fbdc58
Ericlys Moreira <ericlysm@gmail.com>
2022-11-28 15:24:54 -0300

    feat: client middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
@@ -20,6 +21,7 @@ routes.post("/client/authenticate", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
-routes.post("/delivery", createDeliveryController.handle);
+
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle);
 
 export { routes };
