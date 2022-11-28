bdcd2fc785429686e15be652f5f206748b2c7fa0
Ericlys Moreira <ericlysm@gmail.com>
2022-11-28 17:29:01 -0300

    feat: find deliveries from deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import { CreateDeliveryController } from "./modules/deliveries/useCases/createDe
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { FindAllDeliveriesDeliverymanController } from "./modules/deliveryman/useCases/findAllDeliveries/FindAllDeliveriesDeliverymanController";
 
 const routes = Router();
 
@@ -22,6 +23,7 @@ const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 const updateDeliverymanController = new UpdateDeliverymanController();
 const findAllDeliveriesController = new FindAllDeliveriesController();
+const findAllDeliveriesDeliverymanController = new FindAllDeliveriesDeliverymanController();
 
 routes.post("/client", createClientController.handle);
 
@@ -42,6 +44,9 @@ routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman,
 
 routes.get("/client/deliveries", ensureAuthenticateClient,
   findAllDeliveriesController.handle);
+
+routes.get("/deliveryman/deliveries", ensureAuthenticateDeliveryman,
+  findAllDeliveriesDeliverymanController.handle);
 
 
 export { routes };
