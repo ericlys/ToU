414ba8dd211baa6697e82758e3371e5c6871f79a
Ericlys Moreira <ericlysm@gmail.com>
2022-11-29 13:57:00 -0300

    feat: pagination in available deliveries

--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableController.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableController.ts
@@ -3,8 +3,14 @@ import { FindAllAvailableUseCase } from "./FindAllAvailableUseCase";
 
 export class FindAllAvailableController {
   async handle(request: Request, response: Response) {
+    const { page, limit, search } = request.query;
+
     const findAllAvailableUseCase = new FindAllAvailableUseCase();
-    const deliveries = await findAllAvailableUseCase.execute();
+    const deliveries = await findAllAvailableUseCase.execute({
+      limit: Number(limit) || 10,
+      page: Number(page) || 1,
+      search: String(search || "")
+    });
 
     return response.json(deliveries)
   }
--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -1,15 +1,45 @@
 import { prisma } from "../../../../database/prismaClient";
 
+interface FindAllAvailableUseCaseRequest {
+  page: number;
+  limit: number;
+  search: string;
+}
 
 export class FindAllAvailableUseCase {
-  async execute() {
-    const deliveries = await prisma.deliveries.findMany({
-      where: {
-        end_at: null,
-        id_deliveryman: null,
-      }
-    })
+  async execute({ page, limit, search }: FindAllAvailableUseCaseRequest) {
 
-    return deliveries;
+    const deliveries = await prisma.$transaction([
+      prisma.deliveries.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+        where: {
+          end_at: null,
+          id_deliveryman: null,
+          item_name: {
+            contains: search,
+            mode: "insensitive"
+          }
+        }
+      }),
+      prisma.deliveries.count({
+        where: {
+          end_at: null,
+          id_deliveryman: null,
+          item_name: {
+            contains: search,
+            mode: "insensitive"
+          }
+        }
+      }),
+    ]);
+
+    const total_pages = Math.round(deliveries[1] / limit);
+
+    return {
+      deliveries: deliveries[0],
+      count: deliveries[1],
+      total_pages: total_pages > 0 ? total_pages : 1
+    };
   }
 }
